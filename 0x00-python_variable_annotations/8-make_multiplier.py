b2b13f9c0478b900c78afdbbfa014a698b6955af
kamelx0 <alesaweyk@gmail.com>
2024-10-10 21:59:39 +0300

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-'''Task 7's module.
-'''
-from typing import Union, Tuple
+""" module for make_multiplier() """
+from typing import Callable
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''Converts a key and its value to a tuple of the key and
-    the square of its value.
-    '''
-    return (k, float(v**2))
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    returns a function that multiplies a float by multiplier.
+    """
+    def mul(a: float) -> float:
+        return multiplier * a
+    return mul
